d63a13fe88f65d4678138ff91d003af7c838e25d
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-13 01:16:57 -0400

    Include headers now.

--- utils.py
+++ utils.py
@@ -15,6 +15,10 @@ def bino_data_preprocess(data_fname, err_fname):
 		- data_fname: File address for the image data.
 		- err_fname: File address for the error.
 
+	Return: 
+		- data: Self-explantory.
+		- header: A list of corresponding headers extracted from HDU. 
+
     """
     #---- Loading in the 2D data for St82-1hr
     data2D = fits.open(data_fname)
