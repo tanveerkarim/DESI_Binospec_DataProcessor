93136cf99760e5e7b7d3de767892febb80f22939
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-04-23 23:07:32 -0400

    Initialise function DataProcessor

--- Tanveer/Gaussian_Filter.py
+++ Tanveer/Gaussian_Filter.py
@@ -40,7 +40,11 @@ def SNR_Calculator(wavelength, spatial, err):
     #Calculate SNR at different lambda0 and w
     for i in range(len(wavelength)):
         for j in range(len(width)):
-            lambda0 = wavelength[i]
+            """Potentially solve the loop issue by starting with an 
+			arbitary linewidth w and finding zmax; Then for given zmax,
+			test different w"""
+			
+			lambda0 = wavelength[i]
             modelSpatial = Model((wavelength, lambda0), 1, width[j])
             modelPrime = modelSpatial/err
             
@@ -55,3 +59,16 @@ def SNR_Calculator(wavelength, spatial, err):
         z[i] = lambda0/lambda0_emitted - 1
     
     return SNR, z, width
+	
+def DataProcessor(imagefile, errorfile, idx = 48):
+	"""Uses Jae's code to process BinoSpec data
+	Parameters: imagefile: Location of the 2D spectrum file
+				errorfile: Location of the 2D error file
+				idx: array number to be checked
+	"""
+	from utils import bino_data_preprocess
+	
+	data, header = bino_data_preprocess(imagefile, errorfile)
+	image = data[idx][:, :, 0]
+	err = data[idx][:, :, 1]
+	ivar = 1/err**2 #2D inverse variance
