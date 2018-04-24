837acadb00b671f059d166784641abf4145e834b
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-04-23 23:28:30 -0400

    Finish initial implementation

--- Tanveer/Gaussian_Filter.py
+++ Tanveer/Gaussian_Filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 plt.style.use('ggplot')
 
 def ModelDoublet(params, Amp = 1, std = 0.75): #Default, A = 1, std = 0.75
@@ -64,8 +65,7 @@ def DataProcessor(imagefile, errorfile, idx = 48):
 	"""Uses Jae's code to process BinoSpec data
 	Parameters: imagefile: Location of the 2D spectrum file
 				errorfile: Location of the 2D error file
-				idx: array number to be checked
-	"""
+				idx: array number to be checked"""
 	from utils import bino_data_preprocess
 	
 	data, header = bino_data_preprocess(imagefile, errorfile)
@@ -74,6 +74,7 @@ def DataProcessor(imagefile, errorfile, idx = 48):
 	ivar = 1/err**2 #2D inverse variance
 	
 	#Generate a plot of the 2D spectrum and inverse variance
+	plt.clf()
 	plt.subplot(2, 1, 1)
 	plt.imshow(image, aspect = 'auto',  vmin=-4, vmax=4, cmap = 'gray', interpolation = 'None')
 	plt.colorbar()
@@ -101,9 +102,52 @@ def DataProcessor(imagefile, errorfile, idx = 48):
 	
 	#Generate profile and raw spectrum
 	profile = image.sum(axis=1)
+	plt.clf()
 	plt.plot(profile)
 	plt.savefig("Profile_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
 	
 	spectrumRaw = image.sum(axis=0)
+	plt.clf()
 	plt.plot(wave_grid_600, spectrumRaw)
-	plt.savefig("SpectrumRaw_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
+	plt.savefig("SpectrumRaw_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None
+	
+	
+	#Generate cleaned spectrum
+	spectrumClean = np.sum(np.multiply(image.T, np.multiply(ivar.T, profile)).T, axis = 0) \
+	/np.sum(np.multiply(ivar.T, profile).shape)
+	"""Need to figure out how to construct the 1D error array"""
+	#errClean = 1/np.sqrt(np.sum(np.multiply(ivar.T, profile).T, axis = 0)) 
+	errClean = 0.1 #For now use this dummy value
+	plt.clf()
+	plt.plot(wave_grid_600, spectrumClean)
+	plt.savefig("SpectrumClean_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
+	
+	return wave_grid_600, spectrumClean, errClean
+
+idx = 48
+wavegrid1d, spectra1d, err1d = DataProcessor("../../data/v1/st82-1hr-600/obj_abs_slits_lin.fits", 
+"../../data/v1/st82-1hr-600/obj_abs_err_slits_lin.fits", idx)
+
+start_time = time.time()
+SNR, z, w = SNR_Calculator(wavegrid1d, spectra1d, err1d)
+print("--- %s seconds ---" % (time.time() - start_time)) #Time taken to run code
+
+#Generate grid plots
+ax = sns.heatmap(SNR, linewidth=0.01)#, xticklabels=z, yticklabels=w)
+ax.set(xlabel='Redshift z', ylabel='Line width w')
+plt.clf()
+plt.savefig("w_vs_z_grid1_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
+plt.clf()
+plt.imshow(SNR, aspect='auto')
+plt.colorbar()
+plt.savefig("w_vs_z_grid2_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
+
+#Max SNR
+print('Maximum SNR is ' + str(SNR.max()))
+
+#Plot 1D spectra vs z for SNR.max
+plt.plot(z, SNR[np.where(SNR == SNR.max())[0][0]])
+plt.axhline(7)
+plt.xlabel('z')
+plt.ylabel('SNR')
+plt.savefig("MaxSNR_" + str(idx) + ".pdf", dpi = 600, bbox_inches = None)
