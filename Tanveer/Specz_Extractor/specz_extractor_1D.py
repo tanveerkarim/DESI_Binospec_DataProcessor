c91c5f9314d4964691e3664d30a1086a6f92e753
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-05-22 12:00:30 -0700

    Update ModelO2 function to fix variable assignments

--- Tanveer/Specz_Extractor/specz_extractor_1D.py
+++ Tanveer/Specz_Extractor/specz_extractor_1D.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 def ModelO2(wavelength_array, width, Amp = 1):
-"""Returns the [OII] doublet model
+	"""Returns the [OII] doublet model
 	Parameters: wavelength_array: Full wavelength array of MMT BinoSpec. This is constant.
 				width: width of the Gaussian doublets. This varies.
-				Amp: Amplitude of the Gaussian doublets"""
+				Amp: Amplitude of the Gaussian doublets
+				"""
 
 	Gaussian = lambda x, mean, std: np.exp(-((x - mean)/std)**2)
 	
 	#Values from http://classic.sdss.org/dr6/algorithms/linestable.html
 	separation = (3729.875-3727.092)/2 #separation between lambda0 and the emission lines
-	
-	return Amp*(Gaussian(wavelength_array, lambda0-separation, std) + Gaussian(spectra, lambda0+separation, std))
+	lambda0 = wavelength_array[(len(wavelength_array)-1)//2] #Take the midpoint of the wavelength_array as the centre of the Gaussian doublet
+	return Amp*(Gaussian(wavelength_array, lambda0-separation, width) + Gaussian(wavelength_array, lambda0+separation, width))
 
 
