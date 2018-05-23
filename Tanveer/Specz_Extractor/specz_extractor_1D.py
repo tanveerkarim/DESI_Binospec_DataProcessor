25f34c68293579d32f359a69a79fdd879b3dd611
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-05-23 16:50:36 -0700

    Fix comment intendation error

--- Tanveer/Specz_Extractor/specz_extractor_1D.py
+++ Tanveer/Specz_Extractor/specz_extractor_1D.py
@@ -7,17 +7,18 @@ def ModelO2(wavelength_array, width, Amp = 1):
 	Parameters: wavelength_array: Full wavelength array of MMT BinoSpec. This is constant.
 				width: width of the Gaussian doublets. This varies.
 				Amp: Amplitude of the Gaussian doublets
-				"""
+	"""
 
 	Gaussian = lambda x, mean, std: np.exp(-((x - mean)/std)**2)
-	
+
 	#Values from http://classic.sdss.org/dr6/algorithms/linestable.html
 	separation = (3729.875-3727.092)/2 #separation between lambda0 and the emission lines
-	lambda0 = wavelength_array[(len(wavelength_array)-1)//2] #Take the midpoint of the wavelength_array as the centre of the Gaussian doublet
+	lambda0 = wavelength_array[(len(wavelength_array)-1)//2] #Take the midpoint of the wavelength_array as the centre
+                                                             #of the Gaussian doublet
 	return Amp*(Gaussian(wavelength_array, lambda0-separation, width) + Gaussian(wavelength_array, lambda0+separation, width))
 
 def Window(wavelength_array, ngal, pixel_size, window_size_multiplier):
-"""Returns windows to run the Model function over to speed up calculation
+	"""Returns windows to run the Model function over to speed up calculation
 	Parameters: wavelength_array: Full wavelength array of MMT BinoSpec. This is constant
 				ngal: number of galaxies in a given data file
 				pixel_size: width of pixels in wavelength_array
