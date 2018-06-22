35851b8d2e8ee4b5db6abafb4c26ac035a1abdc5
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-06-22 15:29:13 -0400

    Update Model function

--- Tanveer/Specz_Extractor/specz_extractor_1D.py
+++ Tanveer/Specz_Extractor/specz_extractor_1D.py
@@ -23,20 +23,30 @@ def lambda_to_z(wavelength):
 	
 	return (wavelength/lambda0 - 1)
 
-def ModelO2(wavelength_array, width, Amp = 1):
-	"""Returns the [OII] doublet model
-	Parameters: wavelength_array: Full wavelength array of MMT BinoSpec. This is constant.
-				width: width of the Gaussian doublets. This varies.
-				Amp: Amplitude of the Gaussian doublets
-	"""
+def Model(z, wg2, width, Amp = 1):
+    """Returns Gaussian filter model at redshift z
+    
+    Parameters
+    ----------
+    z: array of redshifts at which the model is being tested
+    wg2: pixel grid of the Window
+    width: width array of the Gaussian doublets
+    Amp: amplitude of the Gaussian doublets
+    
+    Returns
+    --------
+    model: Gaussian models in the range of [z - window_width, z + window_width]
+    """
+    
+    lambda_r27 = 3727.092; lambda_r29 = 3729.875 #rest frame wavelength of the [OII] doublets
+    separation_r = (lambda_r29 - lambda_r27) #separation between the emission lines in rest frame
+    lambda0 = lambda_r27 + separation_r/2 #Midpoint of the gaussian emission lines in restframe
+    lambda_obs = lambda0*(1 + z) #Observed wavelength of of the midpoint
+    Gaussian = lambda x, mean, std: (1/np.sqrt(2*np.pi*std**2))*np.exp(-((x[:, np.newaxis] - mean)/std)**2)
 
-	Gaussian = lambda x, mean, std: np.exp(-((x - mean)/std)**2)
-
-	#Values from http://classic.sdss.org/dr6/algorithms/linestable.html
-	separation = (3729.875-3727.092)/2 #separation between lambda0 and the emission lines
-	lambda0 = wavelength_array[(len(wavelength_array)-1)//2] #Take the midpoint of the wavelength_array as the centre
-															 #of the Gaussian doublet
-	return Amp*(Gaussian(wavelength_array, lambda0-separation, width) + Gaussian(wavelength_array, lambda0+separation, width))
+    model = Amp/2*(Gaussian(wg2, lambda_obs - separation_r, width) + Gaussian(wg2, lambda_obs + separation_r, width))
+        
+    return model
 
 def Window(wavelength_array, ngal, pixel_size, window_size_multiplier):
 		"""Returns windows to run the Model function over to speed up calculation
@@ -68,3 +78,6 @@ def Window(wavelength_array, ngal, pixel_size, window_size_multiplier):
 		nwindow_ndarray = np.repeat(nwindow_array[np.newaxis, :, :], ngal, axis=0)
 
 		return nwindow_ndarray
+		
+
+		
