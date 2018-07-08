e6daec1f0ad216ae10ee87067ce51252e48a310f
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-07-08 18:00:42 -0400

    Add mock catalogue handling functionality

--- Tanveer/Specz_Extractor/plot_spectra1D_gen.py
+++ Tanveer/Specz_Extractor/plot_spectra1D_gen.py
@@ -10,8 +10,26 @@ import os
 if not os.path.exists('results/spectra1d/' + maskname):
 	print('Mask directory does not exist. Creating directory...')
 	os.makedirs('results/spectra1d/' + maskname)
+
+isMock = True
+if(isMock == True):
+	
+	def datareader(maskname):
+		"""Reads mask data for use by the other functions in this module
+		Parameters
+		----------
+		maskname: name of the mask + '-' + grating number
+		"""
 		
-data = datareader(maskname)
+		fname = maskname + '.npz'
+		data = np.load(fname)
+		
+		return data
+	
+	data = datareader(maskname)
+else:
+	data = datareader(maskname)
+
 datarows = len(data['data_ivar'][:, 0, :])
 
 #Time the code
