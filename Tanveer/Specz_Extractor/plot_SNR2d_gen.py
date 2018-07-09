736b892206ff1c32c0e10aaeda6168ac59643169
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2018-07-09 12:18:29 -0400

    Unchanged since the last commit

--- Tanveer/Specz_Extractor/plot_SNR2d_gen.py
+++ Tanveer/Specz_Extractor/plot_SNR2d_gen.py
@@ -1,27 +1,68 @@
-"""This script generates 1D spectra and inverse variance plots for a given mask
+"""
+This script generates:
+1. SNR 2d plots as a function of w and z per slit per mask
+2. txt files of redshifts and widths per mask
+3. histograms of redshifts and widths per mask
 """
 
-from specz_extractor_1D import datareader, plotterSpectra1D
+from specz_extractor_1D import datareader, SNR_calculator, plotterSNR2D, SNRvz, wave_grid
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
 import sys
 maskname = sys.argv[1]
 
 #Check if directory for the mask exists; if not, create it
 import os
-if not os.path.exists('results/spectra1d/' + maskname):
+if not os.path.exists('results/SNR2D/' + maskname):
 	print('Mask directory does not exist. Creating directory...')
-	os.makedirs('results/spectra1d/' + maskname)
-		
+	os.makedirs('results/SNR2D/' + maskname)
+	os.makedirs("results/SNRvsRedshift/" + maskname)
+	os.makedirs("results/PeakZoom/" + maskname)
+	
 data = datareader(maskname)
-datarows = len(data['data_ivar'][:, 0, :])
+image = data['data_ivar'][:, 0, :]
+ivar = data['data_ivar'][:, 1, :]
+datarows = len(image)
 
+#Calculate the signal-to-noise
+z, widths, SNRdata, Ampdata = SNR_calculator(maskname, data)
+
+#Initalise arrays to store redshift and width values
+zmax = np.zeros(datarows)
+wmax = np.zeros(datarows)
+		
 #Time the code
 from time import time
 start = time()
 
+wg = wave_grid(data) #wavelength grid of the 1d spectra
+
 for i in range(1, datarows):
-	plotterSpectra1D(maskname, data, idx = i)
-	
+	plotterSNR2D(maskname, idx = i, z = z, widths = widths, SNRdata = SNRdata)
+	zmax[i-1], wmax[i-1] = SNRvz(maskname, idx = i, z=z, widths=widths, SNRdata=SNRdata, Ampdata=Ampdata, image=image, ivar=ivar, wavelength_grid=wg)
 end = time()
 
 tot_time = end - start
 print(str(tot_time))
+
+#Save redshift and width values in a txt file
+import pandas as pd
+
+df = pd.DataFrame({'max_z':zmax, 'max_w':wmax})
+df.to_csv('results/Max_z_n_width/' + maskname + ".txt")
+
+plt.hist(zmax[~np.isnan(zmax)], bins = 15, facecolor = 'red', alpha = 0.75)
+plt.xlabel('Redshift')
+plt.ylabel('Frequency')
+plt.title('Redshift histogram of ' + maskname)
+plt.savefig('results/histograms/redshift/' + maskname + '.pdf', dpi = 600, bbox_inches = None)
+plt.close()
+
+plt.hist(wmax[~np.isnan(wmax)], bins = 4, facecolor = 'red', alpha = 0.75)
+plt.xlabel('Width')
+plt.ylabel('Frequency')
+plt.title('Width histogram of ' + maskname)
+plt.savefig('results/histograms/width/' + maskname + '.pdf', dpi = 600, bbox_inches = None)
+plt.close()
